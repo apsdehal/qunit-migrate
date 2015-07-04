c7c40c64a591329799e0a74de29a43c4d5ea9c12
Amanpreet Singh <apsdehal@gmail.com>
2015-07-04 12:05:00 -0700

    Add test data for globals modifier

---
+++ tests/data/3/actual.js
@@ -0,0 +1,15 @@
+begin();
+done();
+log();
+testStart();
+testDone();
+moduleStart();
+moduleDone();
+// These won't be changed
+QUnit.begin();
+QUnit.done();
+QUnit.log();
+QUnit.testStart();
+QUnit.testDone();
+QUnit.moduleStart();
+QUnit.moduleDone();
---
+++ tests/data/3/expected.js
@@ -0,0 +1,15 @@
+QUnit.begin();
+QUnit.done();
+QUnit.log();
+QUnit.testStart();
+QUnit.testDone();
+QUnit.moduleStart();
+QUnit.moduleDone();
+// These won't be changed
+QUnit.begin();
+QUnit.done();
+QUnit.log();
+QUnit.testStart();
+QUnit.testDone();
+QUnit.moduleStart();
+QUnit.moduleDone();
