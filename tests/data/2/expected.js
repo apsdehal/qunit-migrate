cb20cac56b23999bd9b37c84143c4d31a03ba754
Amanpreet Singh <apsdehal@gmail.com>
2015-07-04 11:43:54 -0700

    Add 2nd test data

---
+++ tests/data/2/expected.js
@@ -0,0 +1,52 @@
+QUnit.test('Def tests 1');
+QUnit.asyncTest('Def tests 2');
+QUnit.test('Def tests 3');
+QUnit.asyncTest('Def tests 4');
+QUnit.test('Def tests 5', function( assert ) {
+
+});
+
+QUnit.test('Def tests 6', function( assert ) {
+
+});
+
+QUnit.test('Def tests 7', function( assert ){
+
+});
+
+QUnit.asyncTest('Def tests 8', function( assert ) {
+
+});
+
+QUnit.asyncTest('Def tests 9', function( assert ) {
+
+});
+
+QUnit.asyncTest('Def tests 10', function( assert ){
+
+});
+
+// Won't match all these so these would remain as they are
+QUnit.test('Def tests 11', function () {
+
+});
+
+QUnit.test('Def tests 12', function() {
+
+});
+
+QUnit.test('Def tests 13', function(){
+
+});
+
+QUnit.asyncTest('Def tests 14', function () {
+
+});
+
+QUnit.asyncTest('Def tests 15', function() {
+
+});
+
+QUnit.asyncTest('Def tests 16', function(){
+
+});
