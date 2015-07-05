b25d43ea5269e06ba2b5bd27ff5c2d3fc2b3da22
Amanpreet Singh <apsdehal@gmail.com>
2015-07-04 20:19:39 -0700

    Create the main modifier

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-var assertionsModifier = require('modifiers/assertions');
-var definitionsModifier = require('modifiers/definitions');
-var globalsModifier = require('modifiers/globals');
-var testsModifier = require('modifiers/tests');
+var assertionsModifier = require('./modifiers/assertions');
+var definitionsModifier = require('./modifiers/definitions');
+var globalsModifier = require('./modifiers/globals');
+var testsModifier = require('./modifiers/tests');
 
 module.exports = function (err, data, options) {
   if (err) {
@@ -9,7 +9,7 @@ module.exports = function (err, data, options) {
     return err;
   }
 
-  data = assertionModifier(data);
+  data = assertionsModifier(data);
   data = globalsModifier(data);
   data = testsModifier(data);
 
