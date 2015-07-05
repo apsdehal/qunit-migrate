f6266beb9d157c651dd7bb33d4b97295f20769b3
Amanpreet Singh <apsdehal@gmail.com>
2015-07-04 18:27:36 -0700

    Use modifiers in main file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-var assertionModifier = require('modifiers/assertion');
+var assertionsModifier = require('modifiers/assertions');
+var definitionsModifier = require('modifiers/definitions');
+var globalsModifier = require('modifiers/globals');
+var testsModifier = require('modifiers/tests');
 
-module.exports = function (err, data) {
+module.exports = function (err, data, options) {
   if (err) {
     console.log(err);
     return err;
   }
 
   data = assertionModifier(data);
+  data = globalsModifier(data);
+  data = testsModifier(data);
+
+  if (options.definitions) {
+  	data = definitionsModifier(data);
+  }
+
+  return data;
 }
