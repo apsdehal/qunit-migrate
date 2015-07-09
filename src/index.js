d12616d7d9a6b4583a7e1371a946953bada563d9
Amanpreet Singh <apsdehal@gmail.com>
2015-07-08 21:04:50 -0700

    Make more robust file handling and help support

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ var logSymbols = require('log-symbols');
 var doubleQuotes = args.q;
 var definitions = args.d;
 var shouldWeWrite = args.w;
-var files = args._[0].split(',');
+var help = args.h || args.help;
+
+if (help) {
+  console.log(spitHelp());
+  process.exit(0);
+}
+
+var files = args._[0].split(',').map(function (x) {
+  // Append .js to all files even if they don't have one
+  // And strip any extra whitespace
+  return x.toString().trim().split(".js")[0] + ".js";
+});;
+
 var options =  {
 	definitions: definitions,
   doubleQuotes: doubleQuotes
@@ -38,4 +50,14 @@ function writeFile(file, data) {
       console.log(logSymbols.success + ' Wrote ' + file + 'successfully');
      }
   });
+}
+
+function spitHelp() {
+  return 'QUnit Migrate\nMigrate your old QUnit code to new api.\n\n'
+         + 'Usage: qunit-migrate "file1, file2, file3" -d -q -w\n\nOptions:\n'
+         + '-w: If you want to write the output to file, default: false\n'
+
+         + '-d: If you want definitions support, this will try to add QUnit deps to AMD definitions and anonymous requires.\n Keep in mind, qunit-migrate currently doesn\'t check if qunit definition is already present or not.\n'
+
+        + '-q: If you want double quoutes support, by default we convert to single quotes';
 }
