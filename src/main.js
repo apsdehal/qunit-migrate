bf7e0e7e0696bc63711c554b8fef32967c0d2be6
Amanpreet Singh <apsdehal@gmail.com>
2015-10-05 17:46:23 +0530

    Update main source to use async modifier

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ var assertionsModifier = require('./modifiers/assertions');
 var definitionsModifier = require('./modifiers/definitions');
 var globalsModifier = require('./modifiers/globals');
 var testsModifier = require('./modifiers/tests');
+var asyncModifier = require('./modifiers/async');
 
 module.exports = function (data, options) {
   data = assertionsModifier(data);
   data = globalsModifier(data);
   data = testsModifier(data);
+  data = asyncModifier(data);
 
   if (options.definitions) {
   	data = definitionsModifier(data, options);
