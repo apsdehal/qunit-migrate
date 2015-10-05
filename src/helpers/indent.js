1782d9d3a6a7c81eb67b83e27c995fff619e8778
Amanpreet Singh <apsdehal@gmail.com>
2015-10-05 18:52:29 +0530

    Fix indent helper to get indent of next line

--- src/helpers/indent.js
+++ src/helpers/indent.js
@@ -1,10 +1,12 @@
 var detectIndent = require('detect-indent');
 
+// This function will return indent for next line
 module.exports = function (line) {
 	var indent = detectIndent(line);
 	var indentString = '';
-	while(indent--) {
-		indentString += '\t';
+	var amount = indent.amount + 1;
+	while(amount--) {
+		indentString += indent.indent;;
 	}
 
 	return indentString;
