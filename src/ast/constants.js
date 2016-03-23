9a595b06e53ce39bacc0454f808256b44366c2e2
Amanpreet Singh <apsdehal@gmail.com>
2016-03-23 17:21:22 +0530

    Modify main file and add constants

---
+++ src/ast/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'qunit': 'QUnit',
+  'module': 'module'
+}
--- src/ast/main.js
+++ src/ast/main.js
@@ -6,21 +6,8 @@ var code = 'var k = function () { module("Hi") }';
 
 var ast = recast.parse(code);
 
-
-var moduleQUnitExpression = builder.memberExpression(
-  builder.identifier("QUnit"),
-  builder.identifier("module"),
-  false
-);
-
-
 var newAst = estraverse.replace(ast.program, {
   enter: function (node, parent) {
-    console.log(node.type);
-    if (node.type == 'Identifier' && node.name == 'module') {
-      this.break();
-      return moduleQUnitExpression;
-    }
   }
 });
 
